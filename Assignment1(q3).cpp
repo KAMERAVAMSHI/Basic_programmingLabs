dd8ac7a0e14377431e30251f7382abf29ba831c7
Prachi Nandi <prachinandi237@gmail.com>
2021-03-19 23:57:41 +0530

    done

---
+++ Assignment1(q3).cpp
@@ -0,0 +1,15 @@
+//Read your name, roll number and branch and print it.
+
+#include<stdio.h>
+int main()
+{
+	char name[20];
+	char roll[10];
+	char branch[20];
+	printf("enter your name, roll number, branch");
+	scanf("%s%s%s",&name,&roll,&branch);
+	printf("My name is %s",name);
+	printf("My roll number is: ",roll);
+	printf("my branch is",branch);
+	return 0;
+}
