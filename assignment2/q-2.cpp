1b2e21221219dfce1dc90ff1c8ca45540603c39b
Prachi Nandi <prachinandi237@gmail.com>
2021-03-29 21:22:19 +0530

    g

---
+++ assignment2/q-2.cpp
@@ -0,0 +1,22 @@
+//Write a c program to perform following and print the value:
+      //  (a) addition, subtraction, multiplication and division of two integers: 32 &78    
+
+#include<stdio.h>
+int main()
+{
+	int num1=32;
+	int num2=78;
+	int add,sub,mult;
+	float div;
+	add=num1+num2;
+	sub=num2-num1;
+	mult=num1*num2;
+	div=num2/num1;
+	
+	printf("\n Addition is :%d",add);
+	printf("\n Subtraction is: %d",sub);
+	printf("\n multiplication is: %d",mult);
+	printf("\n division is: %d \n",div);
+	
+	return 0;
+}
